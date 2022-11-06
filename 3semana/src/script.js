54e289638fe8c495ee351128c411a8f013ac0cd0
MrNOrOnha <40928699+mrlnoronha@users.noreply.github.com>
2022-11-06 17:15:17 -0300

    new commit

--- 3semana/src/script.js
+++ 3semana/src/script.js
@@ -1,7 +1,35 @@
 let contador = 0;
+let contador1;
+let contador2;
+let escrita1;
 while(contador<=10){
-    console.log("while...",contador)
+    console.log("while..."+contador)
     contador++;
+    contador1 = document.getElementById("contador1"); 
+    contador1.innerHTML = contador; 
 }
+console.log("valor contador..."+contador);
+
+do {
+    console.log("do...while..."+contador);
+    contador ++;
+    contador2 = document.getElementById("contador2"); 
+    contador2.innerHTML = contador; 
+}
+while (contador < 20);
+console.log("valor contador..."+contador);
+
+do {
+    pergunta = window.prompt("Deseja continuar [s/n]")
+
+} while(pergunta !== "n");
+
+do {
+    pergunta = window.prompt("Escreva qualquer coisa");
+    escrita1 = document.getElementById("escrita1");
+    escrita1.innerHTML = pergunta;
+} while(pergunta !== "Qualquer coisa");
+
+
 
 
