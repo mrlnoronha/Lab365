b8ea71131d4f8ef0b98cd5dd873d90c335b2614e
MrNOrOnha <40928699+mrlnoronha@users.noreply.github.com>
2022-11-06 12:29:07 -0300

    commint again

---
+++ javascript/src/javascript.js
@@ -0,0 +1,20 @@
+let letra = 0;
+while (letra <= 10){
+    console.log("letra ... " + letra);
+    letra += 1;
+    
+}
+
+x = 5; // Assign 5 to x
+
+elem = document.getElementById("demo"); // Find an element 
+elem.innerHTML = x;           // Display x in the element
+
+var x; // Declare x
+
+x = "Veio do script js"; // Assign 5 to x
+
+elem = document.getElementById("demoa"); // Find an element 
+elem.innerHTML = x;           // Display x in the element
+
+var x; // Declare x
