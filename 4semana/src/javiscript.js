ae243dd636610cf1489f30a34b4c5af6f29e8a8f
MrNOrOnha <40928699+mrlnoronha@users.noreply.github.com>
2022-11-08 23:18:01 -0300

    COMMINT NOW

---
+++ 4semana/src/javiscript.js
@@ -0,0 +1,13 @@
+
+function perguntaidade(){
+    let nome,idade;
+    nome = prompt("digite a:");
+    idade = prompt("digite a idade:");
+    alert("oi "+nome+"você tem "+idade+" anos");
+}
+function tela(){
+    let tela1;
+    tela1 = window.prompt("terminar s/n");
+    tela1 = window.alert("fim");   
+}
+
