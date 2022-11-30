3fe144e3ca2c279aeba41c1bc0e81e327fabf991
MrNOrOnha <40928699+mrlnoronha@users.noreply.github.com>
2022-11-30 05:28:48 -0300

    com

--- calculadora/src/javascript.js
+++ calculadora/src/javascript.js
@@ -1,52 +1,12 @@
-/*funcao quando clica no teclado
- */
-const expresao = []; 
-function geral(argumento){
-      
-    function clickteclado(argumento){
-        console.log('dentro da funcao clicteclado ',expresao);
-        console.log('mostrar matriz dentro clickteclado ',expresao);
-        return argumento;  
-    }
-    if(argumento=="apaga"){
-        document.getElementById("clicktecladotelaexemplo").innerHTML = argumento;
-          
-        console.log('numero cliclado ',argumento);
-        console.log('mostrar matriz dentro geral ',expresao);
-        console.log(expresao);
-
-        expresao.pop(argumento);
-        clickteclado(argumento);
-
-        let txt = "";
-        expresao.forEach(myFunction);
-        document.getElementById("demo").innerHTML = txt;
-
-        function myFunction(value, index, array) {
-            txt += value; 
-        }
-    }else{
-        document.getElementById("clicktecladotelaexemplo").innerHTML = argumento;
-        
-        console.log('numero cliclado ',argumento);
-        console.log('mostrar matriz dentro geral ',expresao);
-        console.log(expresao);
-
-        expresao.push(argumento);
-        clickteclado(argumento);
-
-        let txt = "";
-        expresao.forEach(myFunction);
-        document.getElementById("demo").innerHTML = txt;
-
-        function myFunction(value, index, array) {
-            txt += value; 
-        }
-
-    }
-
-};
-
+var numero ="";
+var valor="";
+var expressao = [];
+function clica(valor){
+    console.log("....dentro da funcao, clicado: ",valor); 
+    
+    document.getElementById("expressao").innerHTML = valor; 
+}
+console.log(numero,"....fora da funcao");
 
 
 
