723026b70f874828daf45f2f503c8c92a92e3fbf
MrNOrOnha <40928699+mrlnoronha@users.noreply.github.com>
2022-12-12 00:59:32 -0300

    mudancas

--- calculadora/src/javascript.js
+++ calculadora/src/javascript.js
@@ -1,12 +1,15 @@
 var numero ="";
 var valor="";
-var expressao = [];
+var vetor = [];
 function clica(valor){
-    console.log("....dentro da funcao, clicado: ",valor); 
-    
+    console.log("....dentro da funcao, clicado: ",valor);     
     document.getElementById("expressao").innerHTML = valor; 
+    document.getElementById("vertorexpressao").innerHTML = vetor.push(valor); 
+    console.log(numero,"....numero dentro da funcao");
+    console.log(vetor,"....vetor dentro da funcao");
+    console.log(valor,"....valor dentro da funcao");
 }
-console.log(numero,"....fora da funcao");
+
 
 
 
